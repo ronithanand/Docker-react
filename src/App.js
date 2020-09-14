0c0b3356a35b9832490a024f8d0cad5e8e7ac03b
unknown <anand.ronith@gmail.com>
2020-09-14 15:47:22 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>WELCOME TO REACT APP</h1>
+        <p>
+          learning<code>Hi There whats</code> autoupdate sequence.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
